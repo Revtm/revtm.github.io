7c2845ac18cbadfcc9afe8f1f63a12c74ff8b6fb
Revtm <kykylizaro98@gmail.com>
2019-09-15 08:06:59 +0700

    setting bootstrap

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Rekam Stok</title>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+
+    <link rel="stylesheet" href="Bootstrap/css/bootstrap.css">
+
+    <style type="text/css">
+
+
+      body {
+        font-family: arial, verdana, sans-serif;
+        color: #665544;
+        padding: 10px;
+      }
+
+      .page{
+        font-family: Courier;
+        border: 1px solid #665544;
+        background-color: #efefef;
+        padding: inherit;
+      }
+    </style>
+
+
+  </head>
+  <body>
+    <div class="page">
+      <h1>Input Jumlah Adonan</h1>
+      <p>input jumlah adonan di sini, jumlah kebutuhan bahan akan ditampilkan</p>
+
+
+    </div>
+    <script src="Bootstrap/jquery-3.4.1.min.js"></script>
+    <script src="Bootstrap/popper.min.js"></script>
+    <script src="Bootstrap/js/bootsrap.js"></script>
+  </body>
+</html>
