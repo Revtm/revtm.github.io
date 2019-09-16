4c38fcd723d2e3b2a448b2fdcbe894b37d5bec99
Revtm <kykylizaro98@gmail.com>
2019-09-16 14:18:54 +0700

    halaman utama

--- index.php
+++ index.php
@@ -30,13 +30,95 @@
   </head>
   <body>
     <div class="page">
-      <h1>Input Jumlah Adonan</h1>
-      <p>input jumlah adonan di sini, jumlah kebutuhan bahan akan ditampilkan</p>
+      <h1>Gudang Wimsoes</h1>
 
+      <form >
+        <fieldset class="form-group">
+          <label for="exampleInputEmail1">Jumlah Adonan</label>
+          <input type="number" class="form-control" id="inputAdonan" placeholder="input disini!">
+          <small class="text-muted">input jumlah adonan di sini, jumlah kebutuhan bahan akan ditampilkan</small>
+        </fieldset>
+
+        <button onclick="calc()" type="button" class="btn btn-primary">Submit</button>
+      </form>
+
+    </br>
+
+      <div class="tabel_bahan">
+        <table class="table">
+
+          <tbody>
+            <tr>
+              <th>Perkiraan pcs</th>
+              <td id="pcs"></td>
+            </tr>
+            <tr>
+              <th>Mentega</th>
+              <td id="mtg">0</td>
+            </tr>
+            <tr>
+              <th>Gula</th>
+              <td id="gul">0</td>
+            </tr>
+            <tr>
+              <th>Susu</th>
+              <td id= "skm">0</td>
+            </tr>
+            <tr>
+              <th>Maizena</th>
+              <td id = "mzn">0</td>
+            </tr>
+            <tr>
+              <th>Telur</th>
+              <td id = "tlr">0</td>
+            </tr>
+            <tr>
+              <th>Mika</th>
+              <td id = "mka">0</td>
+            </tr>
+            <tr>
+              <th>P.Segitiga</th>
+              <td id = "pst">0</td>
+            </tr>
+            <tr>
+              <th>Cup</th>
+              <td id = "cup">0</td>
+            </tr>
+            <tr>
+                <th>Gas</th>
+                <td id = "gas">0</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
 
     </div>
     <script src="Bootstrap/jquery-3.4.1.min.js"></script>
     <script src="Bootstrap/popper.min.js"></script>
     <script src="Bootstrap/js/bootsrap.js"></script>
+    <script type="text/javascript">
+      function calc(){
+
+
+        document.getElementById('pcs').innerHTML = Number(document.getElementById('inputAdonan').value) * 40
+
+      document.getElementById('mtg').innerHTML = Number(document.getElementById('inputAdonan').value)   * 100;
+      document.getElementById('gul').innerHTML = Number(document.getElementById('inputAdonan').value)   * 95;
+      document.getElementById('skm').innerHTML = Number(document.getElementById('inputAdonan').value)   * 90;
+      document.getElementById('mzn').innerHTML = Number(document.getElementById('inputAdonan').value)  * 37.5;
+      document.getElementById('tlr').innerHTML = Number(document.getElementById('inputAdonan').value)   * 5;
+      document.getElementById('mka').innerHTML = Number(document.getElementById('inputAdonan').value)   * 40;
+      document.getElementById('pst').innerHTML = Number(document.getElementById('inputAdonan').value)   * 2;
+      document.getElementById('cup').innerHTML = Number(document.getElementById('inputAdonan').value)   * 40;
+
+      if(Number(document.getElementById('inputAdonan').value)%2 == 1){
+        document.getElementById('gas').innerHTML = (Number(document.getElementById('inputAdonan').value) + 1 ) / 2;
+
+      }else{
+        document.getElementById('gas').innerHTML = (Number(document.getElementById('inputAdonan').value)) / 2;
+
+      }
+      }
+    </script>
   </body>
 </html>
